8651cbe47e1b7f5f0bba9ccbe0fc2cd793586f82
Reece-DB <41407050@newdur.ac.uk>
2026-09-22 11:42:36 +0100

    Add files via upload

---
+++ clicker/script.js
@@ -0,0 +1,24 @@
+let cookies = 0
+
+const counter = document.getElementById('counter')
+const cookieButton = document.getElementById('cookie-button')
+   
+cookieButton.addEventListener('click', function() {
+    cookies= cookies + clickPower;
+        counter.textContent =
+    "cookies: " + cookies;
+}
+);
+
+let clickPower = 1;
+
+const multiplierBtn = document.getElementById('multiplier-btn');
+
+multiplierBtn.addEventListener(
+"click", function() {
+    if (cookies >= 50) {
+    cookies = cookies - 50;
+      clickPower = clickPower + 1;
+      counter.textContent = "cookies: " + cookies;
+}
+});
